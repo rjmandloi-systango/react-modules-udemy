e47ed477a0aff3442c238c73f6fecae34131463d
rjmandloi-systango <rjmandloi@isystango.com>
2022-07-21 19:34:48 +0530

    implement add to cart and removal logic

---
+++ cart-app-using-redux/src/components/Cart/CartButton.js
@@ -0,0 +1,18 @@
+import classes from "./CartButton.module.css";
+import { uiSliceActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(uiSliceActions.toggle());
+  };
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
