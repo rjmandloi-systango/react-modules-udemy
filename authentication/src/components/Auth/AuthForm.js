e9d72155124a666a1486d046aef1373979015007
rjmandloi-systango <rjmandloi@isystango.com>
2022-07-28 14:55:39 +0530

    feat: login , logout , automatically logout after 1 hour

--- authentication/src/components/Auth/AuthForm.js
+++ authentication/src/components/Auth/AuthForm.js
@@ -1,46 +1,77 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
+import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
+
 const AuthForm = () => {
+  const history = useHistory();
+
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authContext = useContext(AuthContext);
+
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    const email = emailInputRef.current.value;
-    const password = passwordInputRef.current.value;
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: Add validation
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBDsX4YX-Cbrtqx9PFExmObeddk-mHqqUU";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBDsX4YX-Cbrtqx9PFExmObeddk-mHqqUU",
-        {
-          method: "POST",
-          body: JSON.stringify({ email, password, returnSecureToken: true }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBDsX4YX-Cbrtqx9PFExmObeddk-mHqqUU";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
+          return res.json();
         } else {
           return res.json().then((data) => {
-            let errorMessage = "Authentication failed";
+            let errorMessage = "Authentication failed!";
             if (data?.error?.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+         (new Date().getTime() + (+data.expiresIn * 1000)
+        ));
+        authContext.login(data.idToken , expirationTime.toISOString());
+        history.replace("/");
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
@@ -49,22 +80,22 @@ const AuthForm = () => {
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input ref={emailInputRef} type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
           <input
-            ref={passwordInputRef}
             type="password"
             id="password"
             required
+            ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
           {!isLoading && (
             <button>{isLogin ? "Login" : "Create Account"}</button>
           )}
-          {isLoading && <p>Sending Request...</p>}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
--- authentication/src/components/Profile/ProfileForm.js
+++ authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,46 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
+  const authContext = useContext(AuthContext);
+  const newPasswordRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordRef.current.value;
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBDsX4YX-Cbrtqx9PFExmObeddk-mHqqUU";
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        idToken: authContext.token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: { "Content-Type": "application / json" },
+    }).then((res) => {
+      console.log(res);
+      history.replace("/");
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          ref={newPasswordRef}
+          minLength="7"
+          type="password"
+          id="new-password"
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
